2aea234c0f22d66dee1efc41f1bd3ba92132bce3
Owen Fairchild <2970027@lfschools.net>
2025-10-21 17:08:03 -0500

    Add launch mechanism
    (cherry picked from commit f20a7f459e210065b851f8397caf3c5b3923e863)

---
+++ TeamCode/src/main/java/LaunchMechanism.java
@@ -0,0 +1,30 @@
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "Launch Mechanism", group = "Club")
+public class LaunchMechanism extends OpMode {
+    DcMotor launchLeft;
+    DcMotor launchRight;
+
+    @Override
+    public void init() {
+        launchLeft = hardwareMap.get(DcMotor.class, "ll");
+        launchRight = hardwareMap.get(DcMotor.class, "lr");
+
+        launchRight.setDirection(DcMotor.Direction.REVERSE);
+
+        // FIXME: encoder
+        launchLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        launchRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    @Override
+    public void loop() {
+        float power = gamepad1.right_trigger;
+        launchLeft.setPower(power);
+        launchRight.setPower(power);
+        telemetry.addData("power", power);
+        telemetry.update();
+    }
+}
