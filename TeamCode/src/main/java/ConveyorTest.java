747b23aa1274670ccd5e6b086f3a75b966c0f5ad
Owen Fairchild <2970027@lfschools.net>
2025-10-21 17:18:20 -0500

    Make ConveyorTest
    (cherry picked from commit d5799577c099071e7bd1bd82bb79ea22e1630caa)

---
+++ TeamCode/src/main/java/ConveyorTest.java
@@ -0,0 +1,27 @@
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "Conveyor Test", group = "Robotics Team")
+public class ConveyorTest extends OpMode {
+    Servo ConveyorServo1;
+    Servo ConveyorServo2;
+
+    @Override
+    public void init() {
+        ConveyorServo1 = hardwareMap.get(Servo.class, "c1");
+        ConveyorServo2 = hardwareMap.get(Servo.class, "c2");
+    }
+
+    @Override
+    public void loop() {
+        // TODO: pick better button
+        if (gamepad1.b) {
+            // Motors are continuous
+            // setPosition acts as setPower
+            ConveyorServo1.setPosition(1);
+            ConveyorServo2.setPosition(1);
+        }
+        // FIXME: will it stop even without setting power to 0?
+    }
+}
